abb60f4ce55c641c67110db6a9ad130abcc3d0eb
Biraj Gautam <birajgtm@gmail.com>
2024-07-19 20:24:20 -0400

    Rad3

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Biraj Gautam- Software Developer",
+  title: "RADHWAN TERCHELLA- Software Developer",
   description:
     "This is the portfolio of Biraj Gautam. I am an IT Professional and web/mobile developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
