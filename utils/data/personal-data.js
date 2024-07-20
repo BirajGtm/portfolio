2c7d082115b82a94cc850db1cdf77a495d668750
Biraj Gautam <birajgtm@gmail.com>
2024-07-19 20:23:40 -0400

    rad2

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
-  name: "Biraj Gautam",
+  name: "Radhwan Md Terchella",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is Biraj Gautam. I am a IT professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Radhwan Md Terchella. I am a IT professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'birajgtm@gmail.com',
   phone: '+1 - 437 667 9502',
   address: 'Toronto, ON, Canada ',
